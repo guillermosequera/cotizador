28731d2fd8d4ce3bf2cb8875376ba03bfb644f3a
Guillermo Sequera <sequeracrespo@gmail.com>
2020-09-09 06:22:38 -0300

    adding state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import Header from './components/Header';
+import Formulario from './components/Formulario';
 
+import styled from '@emotion/styled';
+
+const Contenedor = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+const ContenedorFormulario = styled.div`
+  background-color: #ffffff;
+  padding: 3rem;
+`;
 
 function App() {
   return (
-      <Header 
+      <Contenedor>
+        <Header 
       titulo='Cotizador de seguros'
     />
+
+        <ContenedorFormulario>
+            <Formulario />
+        </ContenedorFormulario>
+      </Contenedor>
   );
 }
 
