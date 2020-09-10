ca4ba9e4096fb9145c3282d063301f9dda3b3d48
Guillermo Sequera <sequeracrespo@gmail.com>
2020-09-09 21:48:33 -0300

    functionsForm

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import styled from '@emotion/styled';
+import { obtenerDiferenciaYear, calcularMarca, obtenerPlan } from '../helpers';
 
 const Campo = styled.div`
     display: flex;
@@ -81,16 +82,26 @@ const Formulario = () => {
 
         guardarError(false);
 
+        //una base de 2000
+        let resultado = 2000;
+
         //obtener la diferencia de años
+        const diferencia = obtenerDiferenciaYear(year);
 
         //por cada año hay que restar el 3%
+        resultado -= (( diferencia * 3) * resultado) / 100;
 
         // Americano 15%
         // Asiatico 5%
         // Europeo 30%
 
+        resultado = calcularMarca(marca) * resultado;
+
         // basico aumenta 20%
         // completo 50%
+        const incrementoPlan = obtenerPlan(plan);
+        resultado = parseFloat( incrementoPlan * resultado ).toFixed(2);
+
 
         //total
     }
